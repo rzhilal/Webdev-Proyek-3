88b90c03952a45019ac78f5959a55e636fbe8fca
Zahratul Mardiyah <98735694+zhrtlmrdyh@users.noreply.github.com>
2022-09-26 11:22:43 +0700

    Frontend template

---
+++ Frontend/src/App.vue
@@ -0,0 +1,55 @@
+<template lang="html">
+  <div classs="">
+
+    <h4> List Users </h4>
+
+    <ul v-for="user in users" :key="user.id">
+      <li>{{user.name}}</li>
+    </ul>
+
+  </div>
+
+</template>
+
+<script>
+  import axios from 'axios'
+import { resolveComponents } from 'uri-js'
+
+  export default {
+  name: 'App',
+  Components: {
+    HelloWorld
+  },  
+    data(){
+      return{
+        users: []
+      }
+    },
+    methods:{
+      load(){
+        axios.get('http://localhost:3000/users')
+        .then((res)=>{
+          this.users = res.data
+        })
+        .catch(()=> {
+          alert('error load data')
+        })
+      }
+    },
+    mounted(){
+      this.load()
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
+  
