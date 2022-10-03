cb2b7e509dc0e8e31ba572c6c8c341783d93bdbb
Zahratul Mardiyah <98735694+zhrtlmrdyh@users.noreply.github.com>
2022-10-03 16:33:30 +0700

    Update CRUD
    Semua fitur CRUD sudah dapat dilakukan dan disambungkan antara BE dan FEnya

--- Frontend/src/view/MyHome.vue
+++ Frontend/src/view/MyHome.vue
@@ -3,10 +3,10 @@
  <nav class="navbar bg-light">
   <div class="container-fluid">
     <a class="navbar-brand">BE x FE</a>
-    <form @submit.prevent="postItem" class="d-flex" role="search">
+    <form @submit.prevent="postItem" class="d-flex">
       <input class="form-control me-2" v-model="form.id" name="id" type="hidden" placeholder="id_" aria-label="Search">
-      <input class="form-control me-2" v-model="form.name" name="name" type="text" placeholder="Nama" aria-label="Nama">
-    
+      <input class="form-control me-2" v-model="form.nama_pengeluaran" name="name" type="text" placeholder="Nama Pengeluaran" aria-label="Nama">
+      <input class="form-control me-2" v-model="form.total_pengeluaran" name="name" type="text" placeholder="Total Pengeluaran" aria-label="Total">
       <button class="btn btn-outline-success" v-show="!updateSubmit" type="submit">Input</button>
       <button class="btn btn-outline-success" v-show="updateSubmit" v-on:click="Update(form)" name="button" type="button">Update</button>
     </form>
@@ -21,21 +21,23 @@
             <button type="submit" v-show="!updateSubmit">add</button> 
             <button type="button" v-show="updateSubmit" v-on:click="Update(form)" name="button">Update</button>
         </form> -->
-        <h3>DATA USERS</h3>
+        <h3>DATA TRANSAKSI</h3>
         <div class="table-content">
           <table class="table table-boordered table-light">
                 <thead>
                     <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">NAME</th>
+                        <th scope="col">Tanggal Transaksi</th>
+                        <th scope="col">Nama Transaksi</th>
+                        <th scope="col">Total Pengeluaran</th>
                         <th scope="col">Edit</th>
                         <th scope="col">Delete</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="user in users" :key="user.id">
-                        <td>{{ user.id }}</td>
-                        <td>{{ user.name }}</td>
+                    <tr v-for="user in Transaksi" :key="user.id">
+                        <td>{{ user.tanggal }}</td>
+                        <td>{{ user.nama_pengeluaran }}</td>
+                        <td>{{ user.total_pengeluaran }}</td>
                         <td><button type="button" v-on:click="Edit(user)" name="button">Edit</button></td>
                         <td><button type="button" v-on:click="Delete(user)" name="button">Delete</button></td>
                     </tr>
@@ -52,17 +54,18 @@
         data() {
             return {
                 form: {
-                    id: "",
-                    name: "",
+                    tanggal : "",
+                    nama_pengeluaran : "",
+                    total_pengeluaran : ""
                 },
-                users: [],
+                Transaksi: [],
             };
         },
         methods: {
             getItem() {
-                axios.get('http://localhost:3000/users')
+                axios.get('http://localhost:8888/api/transaksi')
                 .then((response)=> {
-                    this.users = response.data;
+                    this.Transaksi = response.data;
                     console.log(response.data);
                 })
                 .catch((error) => {
@@ -70,46 +73,54 @@
                 });
             },
             postItem() {
-                axios.post('http://localhost:3000/users', this.form)
+                axios.post('http://localhost:8888/api/transaksi', this.form)
                 .then(()=> {
                     this.getItem();
                     this.form.id= "";
                     this.form.name = "";
-                    alert("Berhasil post data")
+                    alert("Berhasil Input data")
                 })
                 .catch((error) => {
-                    console.log("error post data", error);
+                    console.log("error Input data", error);
                 });
             },
             Edit(user) {
                 this.updateSubmit = true;
-                this.form.id = user.id;
-                this.form.name = user.name;
+                this.form._id = user._id;
+                this.form.nama_pengeluaran = user.nama_pengeluaran;
+                this.form.total_pengeluaran = user.total_pengeluaran;
             },
             Update(form) {
                 axios
-                .put(`http://localhost:3000/users/${form.id}`, {
-                    name: this.form.name,
+                .put(`http://localhost:8888/api/transaksi/${form._id}`, {
+                    nama_pengeluaran: this.form.nama_pengeluaran,
+                   total_pengeluaran: this.form.total_pengeluaran,
+
                 })
                 .then(() => {
                     this.getItem();
-                    this.form.id = "";
-                    this.form.name = "";
+                    this.form._id = "";
+                    this.form.nama_pengeluaran ="";
+                    this.form.total_pengeluaran = "";
                     this.updateSubmit = false;
-                    alert("Data terupdate...");
+                    alert("Data terupdate");
                 })
                 .catch((err) => {
                     console.log(err);
                     console.warn();
-                     alert("!Error update data");
+                    // this.updateSubmit = false;
+                    // this.form._id = "";
+                    // this.form.nama_pengeluaran ="";
+                    // this.form.total_pengeluaran = "";
+                     alert("aaaError update data");
                 });
             },
             Delete(user) {
              axios
-                .delete("http://localhost:3000/users/" + user.id)
+                .delete("http://localhost:8888/api/transaksi/" + user._id)
                 .then(() => {
                     this.getItem();
-                    this.form.name = " ";
+                    this.form._id = "";
                     alert("Data berhasil terhapus");
                 })
                 .catch(() => {
